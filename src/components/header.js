9c2ada8439d50cba8977ee0ed824c9500ac62c62
Matias-15 <mcriado.cg@gmail.com>
2021-10-01 01:31:42 -0300

    Adding Bootstrap5 and create component header

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,8 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import { Container, Nav, Navbar } from "react-bootstrap"
+
 
 const Header = ({ siteTitle }) => (
   <header
@@ -9,24 +11,39 @@ const Header = ({ siteTitle }) => (
       marginBottom: `1.45rem`,
     }}
   >
-    <div
+    <div 
       style={{
         margin: `0 auto`,
-        maxWidth: 960,
+        // maxWidth: 960,
         padding: `1.45rem 1.0875rem`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+      <Container>
+        <Navbar expand="lg"  variant="dark">
+          <Link to="/" style={{ textDecoration: 'none' }}>
+            <Navbar.Brand >
+              {siteTitle}
+            </Navbar.Brand>
+          </Link>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse className="justify-content-end" id="navbarScroll">
+            <Nav as="ul" className="ml-auto align-items-md-center">
+              <Nav.Item as="li" className="mx-md-2">
+                <Link to="/" style={{ textDecoration: 'none', color: '#fff' }}>
+                Home
+                </Link> 
+              </Nav.Item>
+              <Nav.Item as="li" className="mx-md-2">
+                <Link to="/page-2/" style={{ textDecoration: 'none', color: '#fff' }}>
+                Page 2
+                </Link> 
+              </Nav.Item>
+
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </Container>
+      
     </div>
   </header>
 )
--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+// import "./layout.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -29,10 +29,11 @@ const Layout = ({ children }) => {
       <div
         style={{
           margin: `0 auto`,
-          maxWidth: 960,
+          
           padding: `0 1.0875rem 1.45rem`,
         }}
       >
+
         <main>{children}</main>
         <footer
           style={{
